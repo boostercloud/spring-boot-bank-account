0bf59ef171269312760b1ad14de6b77ec8491c48
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2020-07-21 17:23:10 +0100

    first commit

---
+++ src/main/java/com/booster/demos/sbbankaccount/SbBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.booster.demos.sbbankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBankAccountApplication.class, args);
+	}
+
+}
